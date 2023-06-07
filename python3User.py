c09f12d726653d647471d9588edf486772f0bfa7
Shizcobob <kyle.korinek@outlook.com>
2023-06-07 15:51:17 -0400

    update

---
+++ python3User.py
@@ -0,0 +1,47 @@
+class User:
+
+
+    def __init__(self, first_name, last_name, email, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.age = age
+        self.is_reward_member = False
+        self.gold_card_points = 0
+
+    def display_info(self):
+        print(f"First_name : {self.first_name}\nLast_name : {self.last_name}\nEmail: {self.email}\nAge : {self.age}\nIs_Reward_Member: {self.is_reward_member}\nGold_Card_Points : {self.gold_card_points}" )
+
+    def enroll(self):
+        if self.is_reward_member == True:
+            print("Already Memeber")
+            return
+        else:
+            self.is_reward_member = True
+            self.gold_card_points = 200
+
+    def spendPoints(self, spent):
+        self.gold_card_points =self.gold_card_points - spent
+    
+
+
+Anna = User("Anna", "Last", "Hello@goodbye.com", 33)
+Bob = User("Bob", "Last", "emaily@ohno.com", 55)
+Cindy = User("Cindy", "Last", "emaily@ohno.com", 43)
+
+Anna.spendPoints(50)
+Bob.enroll()
+Bob.spendPoints(80)
+Anna.display_info()
+Bob.display_info()
+Cindy.display_info()
+
+
+
+
+
+
+
+
+
+
